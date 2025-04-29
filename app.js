b3c15d1ceae120a59817b0dc0dce0115c797595a
Phat117 <ntphat110705@gmail.com>
2025-04-29 14:30:12 +0700

    Handling GET Requests

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello World', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.status(200).json({ message: 'Hello World', app: 'Natours' });
+// app.post('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello World', app: 'Natours' });
+// });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 app.listen(PORT, () => {
