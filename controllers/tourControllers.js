b429816a32bdbbc5ce442904509d7e9c24c79e9d
Phat117 <ntphat110705@gmail.com>
2025-05-06 20:39:31 +0700

    Another Way of Creating Documents

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,32 +1,6 @@
 const fs = require('fs');
 const Tour = require('../models/tourModel');
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8'),
-// ); Read file
-
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-
-//   const id = +req.params.id * 1;
-//   if (id > tours.length) {
-//     return res.status(404).json({
-//       status: 'fail',
-//       message: 'Invalid ID',
-//     });
-//   }
-//   next();
-// };
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -56,15 +30,19 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  //   console.log(req.body);
-  // const data = req.body;
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = { id: newId, ...data };
-  //   console.log(newTour);
-  // tours.push(newTour);
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
+    const newTour = await Tour.create(req.body);
 
-  return res.status(201).json({ status: 'success', data: { tour: newTour } });
+    return res.status(201).json({ status: 'success', data: { tour: newTour } });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,12 +4,12 @@ const tourController = require('../controllers/tourControllers');
 
 const router = express.Router();
 
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkBody } =
+const { getAllTours, createTour, getTour, updateTour, deleteTour } =
   tourController;
 
 // router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(checkBody, createTour);
+router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
