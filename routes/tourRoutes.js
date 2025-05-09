050dfbb13905aca179d3c53f923e27d638d9a0d4
Phat117 <ntphat110705@gmail.com>
2025-05-09 21:07:38 +0700

    Aggregation Pipeline: Matching and Grouping calculate from the table

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,11 +11,14 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 } = tourController;
 
 // router.param('id', checkID);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
+
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
