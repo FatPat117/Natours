94d4df41b3d6028e8f640da848676cdd32eaad18
Phat117 <ntphat110705@gmail.com>
2025-05-15 22:22:18 +0700

    Creating New Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/userControllers');
+const AuthController = require('../controllers/authController');
 
 const router = express.Router();
+
+const { signUp } = AuthController;
+
+router.route('/signup').post(signUp);
 
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
   userController;
