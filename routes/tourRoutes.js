12a40a5815117bb79ba68c8670df9e80fd3825b7
Phat117 <ntphat110705@gmail.com>
2025-05-17 22:52:16 +0700

    Protecting Tour Routes:Get Token

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const tourController = require('../controllers/tourControllers');
+const authController = require('../controllers/authController');
 
+//router
 const router = express.Router();
+const { protect } = authController;
 
 const {
   getAllTours,
@@ -21,7 +24,7 @@ router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(protect, getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
