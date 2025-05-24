37422094420a4568641582f42376bdf92858e4a0
Phat117 <ntphat110705@gmail.com>
2025-05-24 23:27:06 +0700

    Authorization: User Roles and Permissions

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -25,6 +25,10 @@ router.route('/tour-stats').get(getTourStats);
 router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
