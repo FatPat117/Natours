106e98a58f17d2a606e314415d0ef4b0d8c36226
Phat117 <ntphat110705@gmail.com>
2025-05-25 22:24:16 +0700

    Password Reset Functionality: Setting New Password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const { promisify } = require('util');
+const crypto = require('crypto');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
@@ -149,4 +150,36 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 4) If user hits the button the verify my account
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  // 1) get user based on the token
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  // 2) if token has not expired, and there is user, set the new password
+  if (!user) {
+    next(new AppError('Token is invalid or has expired', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  // 3) update changedPasswordAt property for the user
+
+  // 4) log the user in, send JWT
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
