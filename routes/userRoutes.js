92fbf84cafa28b5d95e94feb75fd2c089d550ab1
Phat117 <ntphat110705@gmail.com>
2025-05-26 21:45:36 +0700

    Deleting the Current User

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const AuthController = require('../controllers/authController');
 
 const router = express.Router();
 
+// Authentication
 const {
   signUp,
   login,
@@ -19,9 +20,19 @@ router.route('/forgotPassword').post(forgotPassword);
 router.route('/resetPassword/:token').patch(resetPassword);
 router.route('/updatePassword').patch(protect, updatePassword);
 
-const { getAllUsers, createUser, getUser, updateUser, deleteUser, updateMe } =
-  userController;
+// User controller
+const {
+  getAllUsers,
+  createUser,
+  getUser,
+  updateUser,
+  deleteUser,
+  updateMe,
+  deleteMe,
+} = userController;
 router.route('/updateMe').patch(protect, updateMe);
+router.route('/deleteMe').delete(protect, deleteMe);
+
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
