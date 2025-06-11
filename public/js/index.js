457e5a85ebf3f5c2e13c847a17833ba17ed73532
Phat117 <ntphat110705@gmail.com>
2025-06-11 17:00:46 +0700

    Adding Image Uploads to Form

--- public/js/index.js
+++ public/js/index.js
@@ -34,10 +34,15 @@ if (logOutBtn) {
 if (userDataForm) {
   userDataForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const name = document.querySelector('#name').value;
+    const form = new FormData();
+    form.append('name', document.querySelector('#name').value);
+    form.append('email', document.querySelector('#email').value);
+    form.append('photo', document.querySelector('#photo').files[0]);
 
-    updateSettings({ name, email }, 'data');
+    // const email = document.querySelector('#email').value;
+    // const name = document.querySelector('#name').value;
+
+    updateSettings(form, 'data');
   });
 }
 
