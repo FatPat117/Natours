aa8e24c2d8f06fe9ce294ac8aa944cf3713f1459
Phat117 <ntphat110705@gmail.com>
2025-06-16 20:03:48 +0700

    Creating New Bookings on Checkout Success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 const AppError = require('../utils/appError');
@@ -38,4 +40,15 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's unsecure: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour || !user || !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]); // redirect to the same page
 });
